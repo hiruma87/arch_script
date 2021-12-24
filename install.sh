5509f069c5ffdc5a98937b28892b1d35bb300659
hiruma87 <96128631+hiruma87@users.noreply.github.com>
2021-12-24 21:34:42 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,88 @@
+echo "-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+echo -e "\nStarting NTP Daemon...\n"
+
+timedatectl set-ntp true
+
+echo -e "\nDone.\n\n"
+
+echo "-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+echo -e "\nFormatting Partitions...\n"
+
+# wipe file system of the installation destination disk
+wipefs --all /dev/sda
+
+# create a new EFI system partition of size 512 MiB with partition label as "BOOT"
+sgdisk -n 0:0:+512M -t 0:ef00 -c 0:BOOT /dev/sda
+
+# create a new Linux LVM partition on the remaining space with partition label as "ROOT"
+sgdisk -n 0:0:0 -t 0:8e00 -c 0:ROOT /dev/sda
+
+# format partition 1 as FAT32 with file system label "ESP"
+mkfs.fat -F 32 /dev/sda1
+
+# Create logical volume
+pvcreate --dataalignment 1m /dev/sda2
+
+# Create LVM group
+vgcreate volg0 /dev/sda2
+
+# Give partition size
+lvcreate -l 100%FREE volg0 -n lv_root
+
+# Give mod to vol group
+modprobe dm_mod
+
+# Scan Vol group
+vgscan
+
+# Activate Vol group
+vgchange -ay
+
+# format partition 2 as EXT4 with file system label "System"
+mkfs.ext4 /dev/volg0/lv_root
+
+echo -e "\nDone.\n\n"
+
+echo "----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+echo -e "\nModifying Pacman Configuration...\n"
+
+# enable options "color", "ParallelDownloads", "multilib (32-bit) repository"
+sed -i 's/#Color/Color/ ; s/#ParallelDownloads/ParallelDownloads/ ; s/#[multilib]/[multilib]/' /etc/pacman.conf
+
+pacman -Syy
+
+echo -e "\nDone.\n\n"
+
+echo "---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+echo -e "\nMounting Partitions...\n"
+
+# mount the ROOT partition on "/mnt"
+mount /dev/volg0/lv_root /mnt
+
+# create necessary directories
+mkdir -p /mnt/boot/EFI
+
+# mount the EFI partition on "/mnt/boot"
+mount /dev/sda1 /mnt/boot/EFI
+
+lsblk
+
+echo -e "\nDone.\n\n"
+
+echo "-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+echo -e "\nPerforming Pacstrap Operation...\n"
+
+# edit and adjust the "pkgs" file for desired packages (don't worry about any extra white spaces or new lines or comments as they will be omitted using sed and tr)
+pacstrap /mnt base linux linux-zen linux-firmware vim nano bash-completion
+
+echo -e "\nDone.\n\n"
+
+echo "--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+echo -e "\nGenerating FSTab...\n"
+
+genfstab -U /mnt >> /mnt/etc/fstab
+
+echo -e "\nDone.\n\nBase installation is now complete.\n\n"
+cp preinstall.sh /mnt/root
+cp extra.sh /mnt/root
+arch-chroot /mnt
---
+++ preinstall.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+######################################################################################################################
+
+pacman -S wget --noconfirm --needed
+
+echo "Getting the ArcoLinux keys from the ArcoLinux repo"
+
+wget https://github.com/arcolinux/arcolinux_repo/raw/master/x86_64/arcolinux-keyring-20230919-6-any.pkg.tar.zst -O /tmp/arcolinux-keyring-20230919-6-any.pkg.tar.zst
+pacman -U --noconfirm --needed /tmp/arcolinux-keyring-20230919-6-any.pkg.tar.zst
+
+######################################################################################################################
+
+echo "Getting the latest arcolinux mirrors file"
+
+wget https://raw.githubusercontent.com/arcolinux/arcolinux-mirrorlist/master/etc/pacman.d/arcolinux-mirrorlist -O /etc/pacman.d/arcolinux-mirrorlist
+echo '
+#[arcolinux_repo_testing]
+#SigLevel = Required DatabaseOptional
+#Include = /etc/pacman.d/arcolinux-mirrorlist
+
+[arcolinux_repo]
+SigLevel = Required DatabaseOptional
+Include = /etc/pacman.d/arcolinux-mirrorlist
+
+[arcolinux_repo_3party]
+SigLevel = Required DatabaseOptional
+Include = /etc/pacman.d/arcolinux-mirrorlist
+
+[arcolinux_repo_xlarge]
+SigLevel = Required DatabaseOptional
+Include = /etc/pacman.d/arcolinux-mirrorlist' | tee --append /etc/pacman.conf
+
+pacman -Syyu --noconfirm
+
+ln -sf /usr/share/zoneinfo/Asia/Kuala_Lumpur /etc/localtime
+hwclock --systohc
+sed -i 's/#en_US.UTF-8 UTF-8/#en_US.UTF-8 UTF-8/' /etc/locale.gen
+echo 'LANG=en_US.UTF-8' >> /etc/locale.conf
+hostnamectl set-hostname asura
+echo '
+127.0.0.1   localhost
+::1         localhost
+127.0.1.1   asura.localdomain   asura' >> /etc/hosts
+pacman -S intel-ucode grub grubbootmgr networkmanager wireless_tools wpa_supplicant os-prober mtools dosfstools base-devel linux-headers linux-zen-headers nvidia-dkms lvm2 --noconfirm
+pacman -S nvidia ntfs-3g gvfs --noconfirm
+sed -i 's/HOOKS=(base udev autodetect modconf block filesystems keyboard fsck)/HOOKS=(base udev autodetect modconf block lvm2 filesystems keyboard fsck)/' /etc/mkinitcpio.conf
+mkinitcpio -p linux
+mkinitcpio -p linux-zen
+passwd
+useradd -m -g users -G audio,video,network,games,wheel,storage,rfkill -s /bin/bash asura
+passwd asura
+EDITOR=vim visudo
+grub-install --target=x86_64-efi --bootloader-id=ArchLinux --recheck
+echo 'GRUB_DISABLE_OS_PROBER=false' >> /etc/default/grub
+grub-mkconfig -o /boot/grub/grub.cfg
+systemctl enable NetworkManager
+umount -a
+exit
