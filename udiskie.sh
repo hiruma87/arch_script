e6a8fc156a70053c09666469ad7d63356f310d27
hiruma87 <96128631+hiruma87@users.noreply.github.com>
2021-12-30 21:02:41 +0800

    Update udiskie.sh

--- udiskie.sh
+++ udiskie.sh
@@ -1,9 +1,5 @@
 pacman -S udisks2 udiskie
 
-su
-
-cd /root/
-
 echo '# UDISKS_FILESYSTEM_SHARED
 # ==1: mount filesystem to a shared directory (/media/VolumeName)
 # ==0: mount filesystem to a private directory (/run/media/$USER/VolumeName)
@@ -11,9 +7,6 @@ echo '# UDISKS_FILESYSTEM_SHARED
 ENV{ID_FS_USAGE}=="filesystem|other|crypto", ENV{UDISKS_FILESYSTEM_SHARED}="1"' >> /etc/udev/rules.d/99-udisks2.rules
 
 echo 'D /media 0755 root root 0 -' >> /etc/tmpfiles.d/media.conf
-
-echo '#    Path                  Mode UID  GID  Age Argument
-w    /proc/acpi/wakeup     -    -    -    -   USBE' >> /etc/tmpfiles.d/disable-usb-wake.conf
 
 echo '### Automatically switch to newly-connected devices
 load-module module-switch-on-connect' >> /etc/pulse/default.pa
