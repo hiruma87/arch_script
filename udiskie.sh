5cd331e063ad79c5570804c5a66712fbfe246dfe
hiruma87 <96128631+hiruma87@users.noreply.github.com>
2021-12-31 19:45:00 +0800

    Update udiskie.sh

--- udiskie.sh
+++ udiskie.sh
@@ -8,5 +8,8 @@ ENV{ID_FS_USAGE}=="filesystem|other|crypto", ENV{UDISKS_FILESYSTEM_SHARED}="1"' 
 
 echo 'D /media 0755 root root 0 -' >> /etc/tmpfiles.d/media.conf
 
+echo '#    Path                  Mode UID  GID  Age Argument
+w    /proc/acpi/wakeup     -    -    -    -   USBE' >> /etc/tmpfiles.d/disable-usb-wake.conf
+
 echo '### Automatically switch to newly-connected devices
 load-module module-switch-on-connect' >> /etc/pulse/default.pa
