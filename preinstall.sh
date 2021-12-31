0cecf4ade5fdbbc438e6c4f0340772a52303e406
hiruma87 <96128631+hiruma87@users.noreply.github.com>
2021-12-31 19:36:12 +0800

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -32,31 +32,97 @@ SigLevel = Required DatabaseOptional
 Include = /etc/pacman.d/arcolinux-mirrorlist' | tee --append /etc/pacman.conf
 
 vim /etc/pacman.conf
+
 pacman -Syyu --noconfirm
 
 ln -sf /usr/share/zoneinfo/Asia/Kuala_Lumpur /etc/localtime
+
 hwclock --systohc
+
 sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+
 locale-gen
+
 echo 'LANG=en_US.UTF-8' >> /etc/locale.conf
+
 hostnamectl set-hostname asura
+
 echo '127.0.0.1       localhost
 ::1             localhost
 127.0.1.1       asura.localdomain       asura
 ff02::1         ip6-allnodes
 ff02::2         ip6-allrouters' >> /etc/hosts
-pacman -S intel-ucode grub efibootmgr networkmanager wireless_tools wpa_supplicant os-prober mtools dosfstools base-devel linux-headers linux-zen-headers lvm2 --noconfirm
-pacman -S ntfs-3g gvfs --noconfirm
+
+func_install() {
+	if pacman -Qi $1 &> /dev/null; then
+		tput setaf 2
+  		echo "###############################################################################"
+  		echo "################## The package "$1" is already installed"
+      	echo "###############################################################################"
+      	echo
+		tput sgr0
+	else
+    	tput setaf 3
+    	echo "###############################################################################"
+    	echo "##################  Installing package "  $1
+    	echo "###############################################################################"
+    	echo
+    	tput sgr0
+    	sudo pacman -S --noconfirm --needed $1
+    fi
+}
+
+###############################################################################
+echo "Installation of the core software"
+###############################################################################
+
+list=(
+base-devel
+intel-ucode
+linux-headers
+linux-zen-headers
+efibootmanager
+os-prober
+mtools
+dosfstools
+networkmanager
+wireless_tools
+wpa_supplicant
+dialog
+ntfs-3g
+gvfs
+)
+
+count=0
+
+for name in "${list[@]}" ; do
+	count=$[count+1]
+	tput setaf 3;echo "Installing package nr.  "$count " " $name;tput sgr0;
+	func_install $name
+done
+
 sed -i 's/HOOKS=(base udev autodetect modconf block filesystems keyboard fsck)/HOOKS=(base udev autodetect modconf block lvm2 filesystems keyboard fsck)/' /etc/mkinitcpio.conf
+
 mkinitcpio -p linux
+
 mkinitcpio -p linux-zen
+
 passwd
+
 useradd -m -g users -G audio,video,network,games,wheel,storage,rfkill -s /bin/bash asura
+
 passwd asura
+
 EDITOR=vim visudo
+
 grub-install --target=x86_64-efi --bootloader-id=ArchLinux --recheck
+
 echo 'GRUB_DISABLE_OS_PROBER=false' >> /etc/default/grub
+
 grub-mkconfig -o /boot/grub/grub.cfg
+
 systemctl enable NetworkManager
+
 umount -a
+
 exit
