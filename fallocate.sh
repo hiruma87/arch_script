bcaca217add6a9302a6495c370dac594c950bc8b
hiruma87 <96128631+hiruma87@users.noreply.github.com>
2022-02-09 13:00:19 +0800

    Update fallocate.sh

--- fallocate.sh
+++ fallocate.sh
@@ -1,5 +1,25 @@
+echo '#################################################################'
+echo 'Creating a swapfile'
+echo '#################################################################'
+sleep 2
 fallocate -l 4GB /swapfile
+sleep 2
+echo
+echo 'Done'
+sleep 2
+echo '#################################################################'
+echo 'Activate swap'
+echo '#################################################################'
+sleep 2
 chmod 600 /swapfile
+sleep 2
 mkswap /swapfile
+sleep 2
+echo '#################################################################'
+echo 'Create swap boot entry'
+echo '#################################################################'
+sleep 2
 echo '/swapfile none swap defaults 0 0' >> /etc/fstab
+sleep 2
 grub-mkconfig -o /boot/grub/grub.cfg
+sleep 2
