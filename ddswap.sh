332064516ab0634d9aed4f0d4b87681dd30e5755
hiruma87 <96128631+hiruma87@users.noreply.github.com>
2022-02-09 13:11:23 +0800

    Update ddswap.sh

--- ddswap.sh
+++ ddswap.sh
@@ -1,10 +1,29 @@
+echo '#######################################################'
+echo 'Enter superuser'
+echo '#######################################################'
+sleep 2
 su
+sleep 2
 cd /root
+sleep 2
+echo '#######################################################'
+echo 'Create a swapfile'
+echo '#######################################################'
+sleep 2
 dd if=/dev/zero of=/swapfile bs=1M count=2048 status=progress
+sleep 2
 chmod 600 /swapfile
+sleep 2
 mkswap /swapfile
+sleep 2
 cp /etc/fstab /etc/fstab.bak
+sleep 2
 echo '/swapfile none swap sw 0 0' >> /etc/fstab
+sleep 2
 cat /etc/fstab
+sleep 2
 swapon -a
+sleep 2
 free -m
+sleep 2
+exit
