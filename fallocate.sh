9849ef16e1eaeaf08a9b34c041b62808386e6ba6
hiruma87 <96128631+hiruma87@users.noreply.github.com>
2022-02-09 13:17:52 +0800

    Update fallocate.sh

--- fallocate.sh
+++ fallocate.sh
@@ -23,17 +23,29 @@ chmod 600 /swapfile
 sleep 2
 mkswap /swapfile
 sleep 2
+echo
+echo 'Done'
+sleep 2
 echo '#################################################################'
 echo 'Create swap fstab entry'
 echo '#################################################################'
 sleep 2
 cp /etc/fstab /etc/fstab.bak
 sleep 2
+echo
+echo 'Done'
+sleep 2
 echo '/swapfile none swap defaults 0 0' >> /etc/fstab
+sleep 2
+echo
+echo 'Done'
 sleep 2
 cat /etc/fstab
 sleep 2
 swapon -a
+sleep 2
+echo
+echo 'Done'
 sleep 2
 free -m
 sleep 2
