0bf63ad2581b565e6d3ab8c9d323fe2edd899e3b
hiruma87 <96128631+hiruma87@users.noreply.github.com>
2022-03-21 20:22:18 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,33 +1,5 @@
-echo "---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
-echo -e "\nFormatting Partitions...\n"
-sleep 3
-
-# wipe file system of the installation destination disk
-wipefs --all /dev/sda
-sleep 3
-
-# create a new EFI system partition of size 512 MiB with partition label as "BOOT"
-sgdisk -n 0:0:+512M -t 0:ef00 -c 0:BOOT /dev/sda
-sleep 3
-
-# create a new Linux x86-64 root (/) partition on the remaining space with partition label as "ROOT"
-sgdisk -n 0:0:0 -t 0:8304 -c 0:ROOT /dev/sda
-sleep 3
-
-# format partition 1 as FAT32 with file system label "ESP"
-mkfs.fat -F 32 /dev/sda1
-sleep 3
-
-# format partition 2
-mkfs.ext4 /dev/sda2
-sleep 3
-
-echo -e "\nDone.\n\n"
-sleep 3
-
-
-echo "-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
 echo -e "\nStarting NTP Daemon...\n"
+sleep 3
 
 timedatectl set-ntp true
 sleep 3
@@ -91,7 +63,7 @@ echo
 
 i=(
 base
-linux
+linux-zen
 linux-firmware
 vim
 bash-completion
@@ -103,7 +75,7 @@ for pkg in "${i[@]}" ; do
 	tput setaf 3;echo "Installing package nr.  "$count " " $pkg;tput sgr0;
 	echo "################################################################"
 sleep 3
-func_install
+func_install $pkg
 done
 
 echo
