d3e0f570d8dd5c22162dbb8d2d1df26271ba5113
hiruma87 <96128631+hiruma87@users.noreply.github.com>
2022-03-22 19:18:30 +0800

    Update norm_part.sh

--- norm_part.sh
+++ norm_part.sh
@@ -11,8 +11,10 @@ sgdisk -n 0:0:+512M -t 0:ef00 -c 0:BOOT /dev/sda
 sleep 3
 
 # create a new Linux x86-64 root (/) partition on the remaining space with partition label as "ROOT"
-sgdisk -n 0:0:0 -t 0:8304 -c 0:ROOT /dev/sda
+sgdisk -n 0:0:+250G -t 0:8304 -c 0:ROOT /dev/sda
 sleep 3
+# Create a new linux home partition
+gdisk -n 0:0:0 -t 0:8304 -c 0:ROOT /dev/sda
 
 # format partition 1 as FAT32 with file system label "ESP"
 mkfs.fat -F 32 /dev/sda1
@@ -22,9 +24,13 @@ sleep 3
 mkfs.ext4 /dev/sda2
 sleep 3
 
+#format patition 3
+mkfs.ext4 /dev/sda3
+
 echo -e "\nDone.\n\n"
 sleep 3
 
+lsblk
 
 echo "-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
 sleep 3
