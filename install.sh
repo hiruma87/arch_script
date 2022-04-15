8302bfde38a1700bf42e34c12471a14a8f5a5b75
hiruma87 <96128631+hiruma87@users.noreply.github.com>
2022-04-15 11:13:34 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,27 +6,6 @@ sleep 3
 
 echo -e "\nDone.\n\n"
 
-echo "-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
-
-echo -e "\nMounting Partitions...\n"
-sleep 3
-# mount the ROOT partition on "/mnt"
-mount /dev/sda2 /mnt
-sleep 3
-#mount home partition
-mkdir -p /mnt/home
-mount /dev/sda3 /mnt/home
-
-# create necessary directories
-mkdir -p /mnt/boot/EFI
-sleep 3
-# mount the EFI partition on "/mnt/boot"
-mount /dev/sda1 /mnt/boot/EFI
-sleep 3
-lsblk
-sleep 3
-echo -e "\nDone.\n\n"
-
 echo "-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
 echo -e "\nModifying Pacman Configuration...\n"
 sleep 3
