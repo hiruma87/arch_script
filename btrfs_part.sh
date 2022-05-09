b0af79188a130e90f785da30a2d8338c722b2a38
hiruma87 <96128631+hiruma87@users.noreply.github.com>
2022-05-09 11:47:10 +0800

    Update btrfs_part.sh

--- btrfs_part.sh
+++ btrfs_part.sh
@@ -12,24 +12,25 @@ sgdisk -n 0:0:+300M -t 0:ef00 -c 0:BOOT /dev/sda
 sleep 3
 
 # create a new Linux x86-64 root (/) partition on the remaining space with partition label as "ROOT"
-sgdisk -n 0:0:+200G -t 0:8304 -c 0:ROOT /dev/sda
+#sgdisk -n 0:0:+200G -t 0:8304 -c 0:ROOT /dev/sda
+sgdisk -n 0:0:0 -t 0:8304 -c 0:ROOT /dev/sda
 sleep 3
 # Create a new linux home partition
-sgdisk -n 0:0:0 -t 0:8304 -c 0:HOME /dev/sda
-sleep 3
+#sgdisk -n 0:0:0 -t 0:8304 -c 0:HOME /dev/sda
+#sleep 3
 mkfs.vfat -F 32 /dev/sda1
 sleep 3
 mkfs.btrfs -F /dev/sda2
 sleep 3
-mkfs.btrfs -F /dev/sda3
+#mkfs.btrfs -F /dev/sda3
 mount /dev/sda2 /mnt
 sleep 3
 btrfs sub cr /mnt/@
-sleep 3
-umount /mnt
-sleep 3
-mount /dev/sda3 /mnt
-sleep 3
+#sleep 3
+#umount /mnt
+#sleep 3
+#mount /dev/sda3 /mnt
+#sleep 3
 btrfs sub cr /mnt/@home
 sleep 3
 umount /mnt
@@ -40,7 +41,7 @@ mkdir -p /mnt/{boot/efi,home}
 sleep 3
 mount /dev/sda1 /mnt/boot/efi/
 sleep 3
-mount -o noatime,space_cache=v2,ssd,compress=zstd,subvol=@home /dev/sda3 /mnt/home
+mount -o noatime,space_cache=v2,ssd,compress=zstd,subvol=@home /dev/sda2 /mnt/home
 lsblk
 sleep 3
 echo -e "\nDone.\n\n"
