6a4e545b637b778f3fde4779e5146b1b406ee2bb
hiruma87 <96128631+hiruma87@users.noreply.github.com>
2022-07-09 00:08:35 +0800

    Update 1_xfce.sh

--- xfce/1_xfce.sh
+++ xfce/1_xfce.sh
@@ -16,119 +16,32 @@
 ###############################################################################
 
 
-###############################################################################
-#
-#   DECLARATION OF FUNCTIONS
-#
-###############################################################################
+tput setaf 5;
+echo "################################################################"
+echo "Run installation script"
+echo "################################################################"
+echo;tput sgr0
 
-
-func_install() {
-	if pacman -Qi $1 &> /dev/null; then
-		tput setaf 2
-  		echo "###############################################################################"
-  		echo "################## The package "$1" is already installed"
-      	echo "###############################################################################"
-      	echo
-		tput sgr0
-	else
-    	tput setaf 3
-    	echo "###############################################################################"
-    	echo "##################  Installing package "  $1
-    	echo "###############################################################################"
-    	echo
-    	tput sgr0
-    	sudo pacman -S --noconfirm --needed $1
-    fi
-}
-
-###############################################################################
-echo "Installation of the core software"
-###############################################################################
-
-list=(
-#org-sercer and desktop
-xorg-server
-lightdm
-lightdm-gtk-greeter
-lightdm-gtk-greeter-settings
-xfce4
-xfce4-goodies
-#printer
-cups
-gutenprint
-#misc
-qbittorrent
-python-pip
-galculator
-easy-rsa
-udiskie
-keepassxc
-#audio
-pulseaudio
-pulseaudio-alsa
-pavucontrol
-alsa-firmware
-alsa-lib
-alsa-plugins
-alsa-utils
-playerctl
-volumeicon
-#bluetooth
-pulseaudio-bluetooth
-bluez
-bluez-libs
-bluez-utils
-blueberry
-#GP
-nvidia-dkms
-#archive
-xarchiver
-unzip
-unrar
-#network
-network-manager-applet
-networkmanager-openvpn
-#web browser
-firefox
-vivaldi
-vivaldi-ffmpeg-codecs
-#wine
-wine-staging
-wine-mono
-wine-gecko
-#fonts
-awesome-terminal-fonts
-adobe-source-sans-fonts
-cantarell-fonts
-noto-fonts
-ttf-bitstream-vera
-ttf-dejavu
-ttf-droid
-ttf-hack
-ttf-inconsolata
-ttf-liberation
-ttf-roboto
-ttf-ubuntu-font-family
-tamsyn-font
-)
-
-for name in "${list[@]}" ; do
-	count=$[count+1]
-	echo "################################################################"
-	tput setaf 3;echo "Installing package nr.  "$count " " $name;tput sgr0;
-	echo "################################################################"
-	sleep 5
-	func_install $name
-done
+sh 2_xorg.sh
+sh 3_graphic.sh
+sh 4_misc.sh
+sh 5_network.sh
+sh 6_audio.sh
+sh 7_bluetooth.sh
+sh 8_browser.sh
+sh 9_printer.sh
+sh 10_wine.sh
+sh 11_fonts.sh
 
 ###############################################################################
 
 
-tput setaf 5;echo "################################################################"
+tput setaf 5;
+echo "################################################################"
 echo "Enabling lightdm as display manager"
 echo "################################################################"
 echo;tput sgr0
+
 sudo systemctl enable lightdm.service
 sudo systemctl enable cups.service
 sudo systemctl enable bluetooth.service
