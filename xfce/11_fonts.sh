38ad93bc44582fa183dea1c530780ba9d4908b6d
hiruma87 <96128631+hiruma87@users.noreply.github.com>
2022-07-09 00:04:14 +0800

    Create 11_fonts.sh

---
+++ xfce/11_fonts.sh
@@ -0,0 +1,48 @@
+func_install() {
+	if pacman -Qi $1 &> /dev/null; then
+		tput setaf 2
+  		echo "###############################################################################"
+  		echo "################## The package "$1" is already installed"
+      	echo "###############################################################################"
+      	echo
+		tput sgr0
+	else
+    	tput setaf 3
+    	echo "###############################################################################"
+    	echo "##################  Installing package "  $1
+    	echo "###############################################################################"
+    	echo
+    	tput sgr0
+    	sudo pacman -S --noconfirm --needed $1
+    fi
+}
+
+###############################################################################
+echo "Installation of the core software"
+###############################################################################
+
+list=(
+#fonts
+awesome-terminal-fonts
+adobe-source-sans-fonts
+cantarell-fonts
+noto-fonts
+ttf-bitstream-vera
+ttf-dejavu
+ttf-droid
+ttf-hack
+ttf-inconsolata
+ttf-liberation
+ttf-roboto
+ttf-ubuntu-font-family
+tamsyn-font
+)
+
+for name in "${list[@]}" ; do
+	count=$[count+1]
+	echo "################################################################"
+	tput setaf 3;echo "Installing package nr.  "$count " " $name;tput sgr0;
+	echo "################################################################"
+	sleep 5
+	func_install $name
+done
