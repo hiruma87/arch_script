9b25162f22c97c70c9bc46ea4a0ff66f770ab6db
hiruma87 <96128631+hiruma87@users.noreply.github.com>
2022-07-18 15:08:54 +0800

    Create 02_xorg_xfce.sh

---
+++ Desktop/02_xorg_xfce.sh
@@ -0,0 +1,43 @@
+func_install() {
+	if pacman -Qi $1 &> /dev/null; then
+		tput setaf 2
+  		echo "###############################################################################"
+  		echo "################## The package "$1" is already installed"
+      	echo "###############################################################################"
+      	echo
+		tput sgr0
+	else
+    	tput setaf 3
+    	echo "###############################################################################"
+    	echo "##################  Installing package "  $1
+    	echo "###############################################################################"
+    	echo
+    	tput sgr0
+    	sudo pacman -S --noconfirm --needed $1
+    fi
+}
+
+###############################################################################
+echo "Installation of the core software"
+###############################################################################
+
+list=(
+#org-sercer and desktop
+xorg-server
+lightdm
+lightdm-gtk-greeter
+lightdm-gtk-greeter-settings
+xfce
+xfce4-goodies
+network-manager-applet
+galculator
+)
+
+for name in "${list[@]}" ; do
+	count=$[count+1]
+	echo "################################################################"
+	tput setaf 3;echo "Installing package nr.  "$count " " $name;tput sgr0;
+	echo "################################################################"
+	sleep 5
+	func_install $name
+done
