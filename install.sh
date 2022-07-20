c138b9b488ab196309c54a16fc3fa8eb70d98d9b
hiruma87 <96128631+hiruma87@users.noreply.github.com>
2022-07-20 16:09:09 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,34 +1,34 @@
 echo -e "\nStarting NTP Daemon...\n"
-sleep 3
+sleep 1
 
 timedatectl set-ntp true
-sleep 3
+sleep 1
 
 echo -e "\nDone.\n\n"
 
 echo "-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
 echo -e "\nModifying Pacman Configuration...\n"
-sleep 3
+sleep 1
 # enable options "color", "ParallelDownloads", "multilib (32-bit) repository"
 sed -i 's #Color Color ; s #ParallelDownloads ParallelDownloads ; s #\[multilib\] \[multilib\] ; /\[multilib\]/{n;s #Include Include }' /etc/pacman.conf
-sleep 3
+sleep 1
 pacman -Sy
 echo -e "\nDone.\n\n"
-sleep 3
+sleep 1
 
 echo "------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
 
 echo -e "\nPerforming Pacstrap Operation...\n"
-sleep 3
+sleep 1
 func_install() {
-	if pacstrap /mnt -Qi $1 &> /dev/null; then
-		tput setaf 2
-  		echo "###############################################################################"
-  		echo "################## The package "$1" is already installed"
+   if pacstrap /mnt -Qi $1 &> /dev/null; then
+	tput setaf 2
+  	echo "###############################################################################"
+  	echo "################## The package "$1" is already installed"
       	echo "###############################################################################"
       	echo
-		tput sgr0
-	else
+	tput sgr0
+   else
     	tput setaf 3
     	echo "###############################################################################"
     	echo "##################  Installing package "  $1
@@ -59,7 +59,7 @@ for pkg in "${i[@]}" ; do
 	echo "################################################################"
 	tput setaf 3;echo "Installing package nr.  "$count " " $pkg;tput sgr0;
 	echo "################################################################"
-sleep 3
+sleep 1
 func_install $pkg
 done
 
@@ -67,16 +67,16 @@ echo
 echo "Done"
 echo
 
-sleep 3
+sleep 1
 echo "--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
 echo -e "\nGenerating FSTab...\n"
 echo
 genfstab -U /mnt >> /mnt/etc/fstab
-sleep 3
+sleep 1
 cat /mnt/etc/fstab
-sleep 3
+sleep 1
 echo -e "\nDone.\n\nBase installation is now complete.\n\n"
-sleep 3
+sleep 1
 curl https://raw.githubusercontent.com/hiruma87/arch_script/main/preinstall.sh -o preinstall.sh
 cp -rf preinstall.sh /mnt
 arch-chroot /mnt
