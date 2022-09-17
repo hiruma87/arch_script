642da7008cdc0f7ebecc162317e32fac100767c9
hiruma87 <96128631+hiruma87@users.noreply.github.com>
2022-09-17 18:15:34 +0800

    Update timedate.sh

--- Desktop/timedate.sh
+++ Desktop/timedate.sh
@@ -1,5 +1,5 @@
 # https://www.archlinuxuser.com/2013/06/how-to-setup-time-date-gui-on-archlinux.html
-sudo pacman -S gnome-control-center --nocomfirm --needed
+sudo pacman -S gnome-control-center --noconfirm --needed
 sudo echo '[Desktop Entry]
 Name=Time Settings
 Exec=gnome-control-center datetime
