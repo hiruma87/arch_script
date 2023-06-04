4481790fa6dbd8da8236196ced25f8e4ce89a89c
hiruma87 <96128631+hiruma87@users.noreply.github.com>
2023-06-04 15:42:33 +0800

    Delete timedate.sh

--- Desktop/timedate.sh
+++
@@ -1,8 +0,0 @@
-# https://www.archlinuxuser.com/2013/06/how-to-setup-time-date-gui-on-archlinux.html
-sudo pacman -S gnome-control-center --noconfirm --needed
-sudo echo '[Desktop Entry]
-Name=Time Settings
-Exec=gnome-control-center datetime
-Icon=preferences-system
-Type=Application
-Categories=GTK;Settings;DesktopSettings;' >> /usr/share/applications/timesettings.desktop
