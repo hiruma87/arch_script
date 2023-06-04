8b8d97dd51af255d4e4067722ae7cb6d95ac3c1b
hiruma87 <96128631+hiruma87@users.noreply.github.com>
2023-06-04 15:53:34 +0800

    Update udiskie.sh

--- Desktop/udiskie.sh
+++ Desktop/udiskie.sh
@@ -5,3 +5,5 @@ echo '# UDISKS_FILESYSTEM_SHARED
 ENV{ID_FS_USAGE}=="filesystem|other|crypto", ENV{UDISKS_FILESYSTEM_SHARED}="1"' >> /etc/udev/rules.d/99-udisks2.rules
 
 echo 'D /media 0755 root root 0 -' >> /etc/tmpfiles.d/media.conf
+
+#echo 'load-module module-switch-on-connect' >> /etc/pulse/default.pa
