4a60fc12c8e0ca135ea94b0421a6908e760963f4
hiruma87 <96128631+hiruma87@users.noreply.github.com>
2024-11-13 15:40:07 +0800

    Update 02-cinnamon.sh

--- Desktop/02-cinnamon.sh
+++ Desktop/02-cinnamon.sh
@@ -1,1 +1,112 @@
+func_install() {
+	if pacman -Qi $1 &> /dev/null; then
+		tput setaf 2
+  		echo "###############################################################################"
+  		echo "################## The package "$1" is already installed"
+      		echo "###############################################################################"
+		echo
+		tput sgr0
+	else
+    		tput setaf 3
+    		echo "###############################################################################"
+    		echo "##################  Installing package "  $1
+    		echo "###############################################################################"
+    		echo
+    		tput sgr0
+    		sudo pacman -S --noconfirm --needed $1
+    	fi
+}
 
+###############################################################################
+echo "Installation of the core software"
+###############################################################################
+
+list=(
+####################################
+#org-sercer and desktop
+####################################
+xorg-server
+lightdm
+lightdm-slick-greeter
+lightdm-gtk-greeter
+lightdm-gtk-greeter-settings
+xfce4
+xfce4-goodies
+####################################
+#GP
+####################################
+#nvidia-lts
+#nvidia-dkms
+nvidia
+####################################
+#misc
+####################################
+qbittorrent
+python-pip
+udiskie
+keepassxc
+xarchiver
+unzip
+unrar
+smplayer
+#flatpak
+neofetch
+####################################
+#network
+####################################
+network-manager-applet
+networkmanager-openvpn
+easy-rsa
+###################################
+#audio
+###################################
+pipewire
+pipewire-pulse
+lib32-pipewire
+pavucontrol
+##################################
+#bluetooth
+##################################
+bluez
+bluez-libs
+bluez-utils
+blueberry
+#################################
+#web browser
+#################################
+firefox
+#################################
+#fonts
+#################################
+ttf-bitstream-vera
+ttf-dejavu
+ttf-droid
+)
+
+for name in "${list[@]}" ; do
+	count=$[count+1]
+	echo "################################################################"
+	tput setaf 3;echo "Installing package nr.  "$count " " $name;tput sgr0;
+	echo "################################################################"
+	sleep 1
+	func_install $name
+done
+
+sudo systemctl enable lightdm.service
+sudo systemctl enable bluetooth.service
+sudo sed -i 's/'#AutoEnable=false'/'AutoEnable=true'/g' /etc/bluetooth/main.conf
+sleep 1
+
+cd "${HOME}"
+cd git
+echo "CLONING: YAY"
+git clone "https://aur.archlinux.org/yay.git"
+cd yay
+makepkg -si --noconfirm
+sleep 1
+
+tput setaf 11;
+echo "################################################################"
+echo "Reboot your system"
+echo "################################################################"
+echo;tput sgr0
