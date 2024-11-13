0d8cef05c11e2a11ca67ccb0501c7bf85427789f
hiruma87 <96128631+hiruma87@users.noreply.github.com>
2024-11-13 14:37:49 +0800

    Update btrfs_part.sh
    20241113

--- btrfs_part.sh
+++ btrfs_part.sh
@@ -8,7 +8,7 @@ wipefs --all /dev/sda
 sleep 1
 
 # create a new EFI system partition of size 512 MiB with partition label as "BOOT"
-sgdisk -n 0:0:+512M -t 0:ef00 /dev/sda
+sgdisk -n 0:0:+1024M -t 0:ef00 /dev/sda
 #sgdisk -n 0:0:+300M -t 0:ef00 -c 0:BOOT /dev/sda
 sleep 1
 
@@ -43,19 +43,47 @@ btrfs sub cr /mnt/@cache
 sleep 1
 btrfs sub cr /mnt/@log
 sleep 1
-btrfs sub cr /mnt/@.home_snap
+btrfs sub cr /mnt/@home/@.snapshots
+sleep 1
+btrfs sub cr /mnt/@opt
+sleep 1
+btrfs sub cr /mnt/@crash
+sleep 1
+btrfs su cr /mnt/@AccountsService
+sleep 1
+btrfs su cr /mnt/@lightdm
+sleep 1
+btrfs su cr /mnt/@lightdm-data
+sleep 1
+#btrfs su cr /mnt/@sddm (or gdm)
+btrfs su cr /mnt/@tmp
+sleep 1
+btrfs su cr /mnt/@images
+sleep 1
+btrfs su cr /mnt/@spool
+sleep 1
+btrfs su cr /mnt/@root
+sleep 1
+btrfs su cr /mnt/@var/@tmp
+sleep 1
 umount /mnt
 sleep 1
 mount -o noatime,space_cache=v2,ssd,compress=zstd,discard=async,subvol=@ /dev/sda2 /mnt
 sleep 1
-mkdir -p /mnt/{boot/efi,home/.snapshots,.swap,.snapshots}
+mkdir -p /mnt/{boot/efi,home/.snapshots,.swap,.snapshots,root,tmp,opt}
 #mkdir -p /mnt/{boot/efi,home,.swap}
-sleep 1
-mkdir -p /mnt/var/{log,cache}
-sleep 1
+sleep 3
+mkdir -p /mnt/var/{log,cache,crash,tmp,spool,lib/{AccountsService,lightdm,lightdm-data,libvirt/images}}
+sleep 3
 mount /dev/sda1 /mnt/boot/efi
 sleep 1
 mount -o noatime,space_cache=v2,ssd,compress=zstd,discard=async,subvol=@home /dev/sda2 /mnt/home
+sleep 1
+mount -o noatime,space_cache=v2,ssd,compress=zstd,discard=async,subvol=@root /dev/sda2 /mnt/root
+sleep 1
+mount -o noatime,space_cache=v2,ssd,compress=zstd,discard=async,subvol=@tmp /dev/sda2 /mnt/tmp
+sleep 1
+mount -o noatime,space_cache=v2,ssd,compress=zstd,discard=async,subvol=@opt /dev/sda2 /mnt/opt
 sleep 1
 mount -o noatime,space_cache=v2,ssd,compress=zstd,discard=async,subvol=@.swap /dev/sda2 /mnt/.swap
 sleep 1
@@ -65,7 +93,21 @@ mount -o noatime,space_cache=v2,ssd,compress=zstd,discard=async,subvol=@log /dev
 sleep 1
 mount -o noatime,space_cache=v2,ssd,compress=zstd,discard=async,subvol=@cache /dev/sda2 /mnt/var/cache
 sleep 1
-mount -o noatime,space_cache=v2,ssd,compress=zstd,discard=async,subvol=@.home_snap /dev/sda2 /mnt/home/.snapshots
+mount -o noatime,space_cache=v2,ssd,compress=zstd,discard=async,subvol=@crash /dev/sda2 /mnt/var/crash
+sleep 1
+mount -o noatime,space_cache=v2,ssd,compress=zstd,discard=async,subvol=@spool /dev/sda2 /mnt/var/spool
+sleep 1
+mount -o noatime,space_cache=v2,ssd,compress=zstd,discard=async,subvol=@var/@tmp /dev/sda2 /mnt/var/tmp
+sleep 1
+mount -o noatime,space_cache=v2,ssd,compress=zstd,discard=async,subvol=@home/@.snapshots /dev/sda2 /mnt/home/.snapshots
+sleep 1
+mount -o noatime,space_cache=v2,ssd,compress=zstd,discard=async,subvol=@AccountsService /dev/sda2 /mnt/var/lib/AccountsService
+sleep 1
+mount -o noatime,space_cache=v2,ssd,compress=zstd,discard=async,subvol=@lightdm /dev/sda2 /mnt/var/lib/lightdm
+sleep 1
+mount -o noatime,space_cache=v2,ssd,compress=zstd,discard=async,subvol=@lightdm-data /dev/sda2 /mnt/var/lib/lightdm-data
+sleep 1
+mount -o noatime,space_cache=v2,ssd,compress=zstd,discard=async,subvol=@images /dev/sda2 /mnt/var/lib/libvirt/images
 sleep 1
 lsblk
 sleep 1
