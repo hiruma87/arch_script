675ebcad37080338763352b769a70f78fc3d7f14
hiruma87 <96128631+hiruma87@users.noreply.github.com>
2024-11-16 06:47:38 +0800

    Update btrfs_part.sh

--- btrfs_part.sh
+++ btrfs_part.sh
@@ -7,6 +7,8 @@ sleep 1
 wipefs --all /dev/sda
 sleep 1
 
+sgdisk -g /dev/sda
+sleep 1
 # create a new EFI system partition of size 512 MiB with partition label as "BOOT"
 sgdisk -n 0:0:+1024M -t 0:ef00 /dev/sda
 #sgdisk -n 0:0:+300M -t 0:ef00 -c 0:BOOT /dev/sda
